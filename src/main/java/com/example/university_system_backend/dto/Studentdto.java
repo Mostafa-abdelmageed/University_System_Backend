0204b7ea11a19aff1d0af060491fbfff2e63d4e0
mostafa <mostafaabdelmageed436@gmail.com>
2026-07-14 09:30:30 +0300

    Initial commit

---
+++ src/main/java/com/example/university_system_backend/dto/Studentdto.java
@@ -0,0 +1,17 @@
+package com.example.university_system_backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Studentdto {
+    private Integer student_id;
+    private String first_name;
+    private String last_name;
+    private String email;
+    private Integer currentlevel;
+    private Integer faculty_id;
+}
---
+++ src/test/java/com/example/university_system_backend/UniversitySystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.university_system_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversitySystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
